fcf76483ca680d91f2cc5806c8b0ab80c53e3e23
Alphaandsew <alfonso.martinez@shrikefound.com>
2020-11-09 14:24:35 -0800

    refactored twist commands
    added read/write functions to make bot commands easier to read

--- index.js
+++ index.js
@@ -1,134 +1,89 @@
 const Discord = require("discord.js");
 const { prefix, token, suits, values } = require("./config.json");
-const {test,cheatCard,draw,shuffle,flip} = require("./twist_functions.js");
 bot = new Discord.Client();
 const fs = require("fs");
 const { type } = require("os");
-bot.twist_decks = require("./TwistDecks.json");
-//
-//bot commands start
-//
+bot.decks = require("./decks.json");
+
+
+
+//bot commands start -----------------------------
+
 bot.commands = new Discord.Collection();
 
-const shuffle_command = {
-  name: "shuffle",
-  description: "shuffles the deck a bit.",
-  execute(message, args) {
-    shuffle(fate_deck);
-    message.channel.send("Fate deck shuffled.");
-  },
-};
-bot.commands.set(shuffle_command.name, shuffle_command);
 
-const flip_command = {
-  name: "flip",
-  description: "flips x numbers of cards",
-  execute(message, args) {
-    num = args[0];
-    sorted = args[1];
-    if (!num > 0) {
-      num = 1;
-    }
-    flipped_cards = flip(fate_deck, num, sorted);
-    faces = [];
-    flipped_cards.forEach((card) => {
-      faces.push(`${card.value} of ${card.suit}`);
-    });
-    message.channel.send("Cards flipped: " + faces.join(","));
-  },
-};
-bot.commands.set(flip_command.name, flip_command);
 
-const create_twist_command = {
+const twist_initialize_command = {
   name: "create",
-  description: "shuffles the deck a bit.",
+  description: "creates a twist deck for the player",
   execute(message, args) {
-    // message.channel.send("creating...");
-    if (args.length <= 3) {
-      message.channel.send(
-        "please enter 4 suits (you entered '" + args + " ')"
-      );
-      return;
-    }
+    let definining_suit = args[0] || "unknown";
+    let ascendant_suit = args[1] || "unknown";
+    let center_suit = args[2] || "unknown";
+    let descendant_suit = args[3] || "unknown";
+    
+    twist_deck = createDeck(definining_suit, ascendant_suit, center_suit, descendant_suit);
 
-    deck = createDeck(args[0], args[1], args[2], args[3]);
-    shuffle(deck);
-    bot.twist_decks[message.author.id] = {
-      cards: deck.cards,
-      hand: deck.hand,
-      discard: deck.discard,
-    };
-    fs.writeFile(
-      "./TwistDecks.json",
-      JSON.stringify(bot.twist_decks, null, 4),
-      (err) => {
-        if (err) throw err;
-        message.channel.send("created. use !show to view your deck.");
-      }
-    );
+    //move 3 cards to to the player's hand
+    draw(twist_deck, 3);
+    writeDeck(message.author.id, twist_deck.cards, twist_deck.hand, twist_deck.discard);
+
   },
 };
-bot.commands.set(create_twist_command.name, create_twist_command);
+bot.commands.set(twist_initialize_command.name, twist_initialize_command);
 
-const show_twist_command = {
-  name: "show",
-  description: "shows the deck",
+
+
+const twist_show_command = {
+  name: "hand",
+  description: "shows a player's twist hand",
   execute(message, args) {
-    hand = bot.twist_decks[message.author.id].hand.map(
+    deck = readDeck(message.author.id)
+    hand = deck.hand.map(
       (card) => card.value + " of " + card.suit
     );
-    message.channel.send("Your hand: " + hand);
-  },
-};
-bot.commands.set(show_twist_command.name, show_twist_command);
+    message.channel.send("Your hand: "+hand);
+  }
+}
+bot.commands.set(twist_show_command.name,twist_show_command)
 
-const cheat_twist_command = {
-  name: "cheat",
-  description: "shows the deck",
-  execute(message, args) {
-    cheated_value = args[0];
-    twist_deck = bot.twist_decks[message.author.id].cards;
-    results = cheatCard(twist_deck, cheated_value);
-    if (results == false) {
-      message.channel.send("couldn't find that card");
-      return;
-    }
-    let cheated_card = results.cheated_card[0],
-      remaining_deck = results.remaining_deck;
-    bot.twist_decks[message.author.id].cards = remaining_deck;
-    discard = bot.twist_decks[message.author.id].discard;
-    discard.push(cheated_card)
-    fs.writeFile(
-      "./TwistDecks.json",
-      JSON.stringify(bot.twist_decks, null, 4),
-      (err) => {
-        if (err) throw err;
-        message.channel.send("created. use !show to view your deck.");
-      }
-    );
 
-    message.channel.send(
-      "cheating with a " + cheated_card.value + " of " + cheated_card.suit
-    );
-  },
-};
-bot.commands.set(cheat_twist_command.name, cheat_twist_command);
 
-const draw_twist_command = {
-  name: "draw",
-  description: "shows the deck",
-  execute(message, args) {
-    deck = bot.twist_decks[message.author.id];
-    message.channel.send("this is the draw command: " + deck);
-  },
-};
-bot.commands.set(draw_twist_command.name, draw_twist_command);
+//bot commands end -----------------------------
 
-//
-//bot commands end
-//
+
+
+
+
+
+function findSuit(string) {
+  char = string.charAt(0).toLowerCase();
+  suit = "";
+  switch (char) {
+    case "m":
+      suit = "masks";
+      break;
+    case "r":
+      suit = "rams";
+      break;
+    case "c":
+      suit = "crows";
+      break;
+    case "t":
+      suit = "tomes";
+      break;
+    default:
+      suit = "unknown";
+      break;
+  }
+  return suit;
+  }
+
+
+
 
 function createDeck(suits, values, center, descendant) {
+  
   var deck = {};
   deck.cards = new Array();
   deck.discard = new Array();
@@ -160,46 +115,96 @@ function createDeck(suits, values, center, descendant) {
       }
     }
   }
-
+  shuffle(deck);
   return deck;
+
 }
 
 
 
-function findSuit(string) {
-  char = string.charAt(0).toLowerCase();
-  suit = "";
-  switch (char) {
-    case "m":
-      suit = "masks";
-      break;
-    case "r":
-      suit = "rams";
-      break;
-    case "c":
-      suit = "crows";
-      break;
-    case "t":
-      suit = "tomes";
-      break;
-    default:
-      suit = "unknown";
-      break;
-  }
-  return suit;
-  }
 
-function writeDeck(deck,author) {
+
+function shuffle(deck) {
+  for (var i = 0; i < 1000; i++) {
+    var randomLocation = Math.floor(Math.random() * deck.cards.length);
+    var temp = deck.cards[0];
+    deck.cards[0] = deck.cards[randomLocation];
+    deck.cards[randomLocation] = temp;
+  }
+}
+
+
+
+
+function writeDeck(authorID, deck, hand, discard) {
   
+  bot.decks[authorID] = {
+    cards: deck,
+    hand: hand,
+    discard: discard
+  }
+
+  fs.writeFile("./decks.json", JSON.stringify(bot.decks,null,4), err => {
+    if (err) throw err;
+  });
+
 }
 
 
 
 
 
+//gets WHOLE deck as object from JSON. has: deck,hand,discard
+function readDeck(authorID) {
+  
+  let deck = bot.decks[authorID];
+  return deck
+
+}
 
 
 
+function draw(deck, numflips) {
+  var drawnCards = [];
+  for (var i = 0; i < numflips; i++) {
+    if (deck.cards.length <= 0) {
+      deck.cards = deck.discard;
+      shuffle(deck);
+    }
+    var drawnCard = deck.cards.shift();
+    drawnCards.unshift(drawnCard);
+  }
+
+  drawnCards.sort((a, b) => {
+    return a.value - b.value;
+  });
+
+  deck.hand = deck.discard.concat(drawnCards);
+  }
+
+
+
+
+
+function flip(deck, numflips, sorted) {
+  var flippedCards = [];
+  for (var i = 0; i < numflips; i++) {
+    if (deck.cards.length <= 0) {
+      deck.cards = deck.discard;
+      shuffle(deck);
+    }
+    var pulledCard = deck.cards.shift();
+    flippedCards.unshift(pulledCard);
+  }
+  if (sorted == "unsorted") {
+  } else {
+    flippedCards.sort((a, b) => {
+      return a.value - b.value;
+    });
+  }
+  deck.discard = deck.discard.concat(flippedCards);
+  return flippedCards;
+}
 
 
 
@@ -208,7 +213,7 @@ function writeDeck(deck,author) {
 
 bot.once("ready", () => {
   fate_deck = createDeck(suits, values);
-  // console.log(fate_deck);
+  writeDeck(0, fate_deck.cards, fate_deck.hand, fate_deck.discard);
   console.log("bot ready!");
 });
 
@@ -233,6 +238,5 @@ bot.on("message", (message) => {
   }
 });
 
-test();
 
 bot.login(token);
