b5b75718276f7ad255b6c121a03944dab3487d89
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-01-31 13:05:46 -0800

    added firebase
    firebase connects bot can now read and write records.

--- index.js
+++ index.js
@@ -11,333 +11,31 @@ const fs = require("fs");
 const { type } = require("os");
 const { Console } = require("console");
 const fetch = require("node-fetch");
-bot.decks = require("./decks.json");
+
+
+//adding firebase
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://lucius-bot-default-rtdb.firebaseio.com/"
+});
+
+let db = admin.database();
+let usersRef = db.ref("users");
 
 //bot commands start -----------------------------
 
 bot.commands = new Discord.Collection();
 
-const fate_shuffle_command = {
-  name: "shuffle",
-  description: "shuffles the twist deck (or fate deck if you're the FM)",
+const fate_deck_initialize_command = {
+  name: "fate_initialize",
+  description: "creates a new fate deck",
   execute(message, args) {
-    console.log(isFM(message.author.id));
-    if (isFM(message.author.id)) {
-      fate_deck = readDeck(0);
-      shuffle(fate_deck);
-      console.log(fate_deck);
-      writeDeck(0, fate_deck.cards, fate_deck.hand, fate_deck.discard);
-      message.channel.send("deck shuffled");
-    } else {
-      twist_deck = readDeck(message.author.id);
-      shuffle(twist_deck);
-      console.log(twist_deck);
-      writeDeck(0, twist_deck.cards, twist_deck.hand, twist_deck.discard);
-      console.log("twist deck shuffled.");
-    }
-  },
-};
-bot.commands.set(fate_shuffle_command.name, fate_shuffle_command);
-
-const old_fate_flip_command = {
-  name: "oldflip",
-  description: "flips a number of cards from the fate deck",
-  execute(message, args) {
-    num = Number(args[0]);
-    if (!num > 0) {
-      num = 1;
-    }
-    console.log(num);
-
-    fate_deck = readDeck(0);
-
-    flippedCards = flip(fate_deck, num).map(
-      (card) => card.value + " of " + card.suit
-    );
-
-    writeDeck(0, fate_deck.cards, fate_deck.hand, fate_deck.discard);
-    message.channel.send("cards flipped: " + flippedCards);
-    console.log(fate_deck.cards.length);
-    console.log(fate_deck.hand.length);
-    console.log(fate_deck.discard.length);
-  },
-};
-bot.commands.set(old_fate_flip_command.name, old_fate_flip_command);
-
-const fate_flip_command = {
-  name: "flip",
-  description: "flips a number of cards from the fate deck",
-  execute(message, args) {
-    const test = async (message, args) => {
-      argString = args.join(" ");
-      let num = Number(argString.match(/\d+/g));
-      if (!num > 0) {
-        num = 1;
-      }
-      let skillName = argString.match(/[\D]+/g);
-      if (skillName) {
-        skillName = skillName.toString().trim().toLowerCase();
-      }
-      console.log(skillName);
-      fate_deck = readDeck(0);
-      const response = await fetch(
-        `https://arcane-scrubland-02167.herokuapp.com/api/${message.author.id}`
-      );
-      const result = await response.json().catch((error) => {
-        console.error(
-          `Character with api_id: '${message.author.id}' Not Found.`
-        );
-        return {
-          error: 404,
-          message: `Character with api_id: '${message.author.id}' Not Found.`,
-        };
-      });
-
-      let skillRank = result[skillName];
-      let activeValue = 0;
-      if (typeof skillRank === "object") {
-        skillRank = result[skillName]["rank"];
-        aspectName = result[skillName]["aspect"];
-        aspectRank = result[aspectName];
-        console.log("skill rank: ", skillRank, " aspect rank: ", aspectRank);
-        activeValue = skillRank + aspectRank;
-      } else if (typeof skillRank === "number") {
-        activeValue = skillRank;
-      } else {
-        //do nothing
-        console.log(`${skillRank} was weird.`);
-      }
-      flippedCards = flip(fate_deck, num).map(
-        (card) => `${card.value}(${card.value + activeValue}) of ${card.suit}`
-      );
-      writeDeck(0, fate_deck.cards, fate_deck.hand, fate_deck.discard);
-      console.log(result);
-      let reply;
-      if (result["error"] === 404) {
-        reply = `**Cards flipped:** ${flippedCards}`;
-      } else {
-        reply = `**${result.name}'s acting value:** ${activeValue} \n**Cards flipped:** ${flippedCards}`;
-      }
-      message.channel.send(reply);
-      console.log(num);
-      console.log(` fate deck length: ${fate_deck.cards.length}`);
-      console.log(` fate hand length: ${fate_deck.hand.length}`);
-      console.log(` fate discard length: ${fate_deck.discard.length}`);
-    };
-    test(message, args);
-  },
-};
-bot.commands.set(fate_flip_command.name, fate_flip_command);
-
-const twist_initialize_command = {
-  name: "create",
-  description: "creates a twist deck for the player",
-  execute(message, args) {
-    let definining_suit = args[0] || "unknown";
-    let ascendant_suit = args[1] || "unknown";
-    let center_suit = args[2] || "unknown";
-    let descendant_suit = args[3] || "unknown";
-    console.log(args);
-    twist_deck = createDeck(
-      definining_suit,
-      ascendant_suit,
-      center_suit,
-      descendant_suit
-    );
-
-    //move 3 cards to to the player's hand
-    draw(twist_deck, 3);
-    writeDeck(
-      message.author.id,
-      twist_deck.cards,
-      twist_deck.hand,
-      twist_deck.discard
-    );
-    //parse hand & discard, show in message to user
-    hand = twist_deck.hand.map((card) => card.value + " of " + card.suit);
-    discard = twist_deck.discard.map((card) => card.value + " of " + card.suit);
-    message.author.send(
-      message.author.username +
-        "'s hand: " +
-        hand +
-        "\n" +
-        message.author.username +
-        "'s discard pile: " +
-        discard
-    );
-  },
-};
-bot.commands.set(twist_initialize_command.name, twist_initialize_command);
-
-const twist_show_command = {
-  name: "show",
-  description: "shows a player's twist hand",
-  execute(message, args) {
-    // if (isFM(message.author.id)) return;
-
-    deck = readDeck(message.author.id);
-    hand = deck.hand.map((card) => card.value + " of " + card.suit);
-    discard = deck.discard.map((card) => card.value + " of " + card.suit);
-    if (args[0] == "hand") {
-      message.author.send(message.author.username + "'s hand: " + hand);
-    } else if (args[0] == "discard") {
-      message.author.send(
-        message.author.username + "'s discard pile: " + discard
-      );
-    } else if (args[0] == "me") {
-      if (args[1] == "the") {
-        if (args[2] == "money") {
-          message
-            .react("💸")
-            .then(message.react("💰"))
-            .then(message.react("💵"));
-        }
-      }
-    } else {
-      message.author.send(
-        message.author.username +
-          "'s hand: " +
-          hand +
-          "\n" +
-          message.author.username +
-          "'s discard pile: " +
-          discard
-      );
-    }
-  },
-};
-bot.commands.set(twist_show_command.name, twist_show_command);
-
-const twist_draw_command = {
-  name: "draw",
-  description: "draws cards from the player's twist deck",
-  execute(message, args) {
-    // if (isFM(message.author.id)) return;
-    twist_deck = readDeck(message.author.id);
-    num = parseInt(args[0]);
-    if (!(num > 0)) {
-      num = 1;
-    }
-    draw(twist_deck, num);
-    writeDeck(
-      message.author.id,
-      twist_deck.cards,
-      twist_deck.hand,
-      twist_deck.discard
-    );
-    //parse hand & discard, show in message to user
-    hand = twist_deck.hand.map((card) => card.value + " of " + card.suit);
-    discard = twist_deck.discard.map((card) => card.value + " of " + card.suit);
-    message.author.send(
-      message.author.username +
-        "'s hand: " +
-        hand +
-        "\n" +
-        message.author.username +
-        "'s discard pile: " +
-        discard
-    );
-  },
-};
-bot.commands.set(twist_draw_command.name, twist_draw_command);
-
-const twist_discard_command = {
-  name: "discard",
-  description: "discards a card from the player's twist deck.",
-  execute(message, args) {
-    var value = parseInt(args[0]);
-    var twist_deck = readDeck(message.author.id);
-    cheatedCard = cheat(twist_deck, value);
-    if (cheatedCard === undefined) {
-      // message.channel.send("You don't have that card.");
-      return;
-    }
-    message.channel.send(
-      message.author.username +
-        " discarded: " +
-        cheatedCard.value +
-        " of " +
-        cheatedCard.suit
-    );
-    //parse hand & discard, show in message to user
-    hand = twist_deck.hand.map((card) => card.value + " of " + card.suit);
-    discard = twist_deck.discard.map((card) => card.value + " of " + card.suit);
-    message.author.send(
-      message.author.username +
-        "'s hand: " +
-        hand +
-        "\n" +
-        message.author.username +
-        "'s discard pile: " +
-        discard
-    );
-  },
-};
-bot.commands.set(twist_discard_command.name, twist_discard_command);
-
-const twist_cheat_command = {
-  name: "cheat",
-  description:
-    "cheats a card; moves it from the player's hand to the discard pile.",
-  execute(message, args) {
-    //card values are ints so we need to parse the argument as an int
-    var value = parseInt(args[0]);
-    var twist_deck = readDeck(message.author.id);
-    cheatedCard = cheat(twist_deck, value);
-    if (cheatedCard === undefined) {
-      // message.channel.send("You don't have that card.");
-      return;
-    }
-    message.channel.send(
-      message.author.username +
-        " cheated with: " +
-        cheatedCard.value +
-        " of " +
-        cheatedCard.suit
-    );
-    writeDeck(
-      message.author.id,
-      twist_deck.cards,
-      twist_deck.hand,
-      twist_deck.discard
-    );
-    //parse hand & discard, show in message to user
-    hand = twist_deck.hand.map((card) => card.value + " of " + card.suit);
-    discard = twist_deck.discard.map((card) => card.value + " of " + card.suit);
-    message.author.send(
-      message.author.username +
-        "'s hand: " +
-        hand +
-        "\n" +
-        message.author.username +
-        "'s discard pile: " +
-        discard
-    );
-  },
-};
-bot.commands.set(twist_cheat_command.name, twist_cheat_command);
-
-const share_api_key = {
-  name: "api",
-  description:
-    "sends you your user id so that you can add it to your character sheet",
-  execute(message, args) {
-    message.author.send(`your user id: ${message.author.id}`);
-  },
-};
-bot.commands.set(share_api_key.name, share_api_key);
-
-// const character_read = {
-//   name: "read",
-//   description: "reads character sheet",
-//   execute(message, args) {
-//     console.log(bot.characters[message.author.id]["flexible"])
-//     let asp = bot.characters[message.author.id]["flexible"]["aspect"]
-//     console.log(asp);
-//     console.log(bot.characters[message.author.id][asp])
-//   }
-// }
-// bot.commands.set(character_read.name,character_read)
+    initializeFateDeck();
+  }
+}
+bot.commands.set(fate_deck_initialize_command.name,fate_deck_initialize_command)
 
 const help_command = {
   name: "help",
@@ -352,38 +50,10 @@ const help_command = {
 };
 bot.commands.set(help_command.name, help_command);
 
-const get_character_command = {
-  name: "review",
-  description: "gets character stuff",
-  execute(message, args) {
-    const async_command = async (message, args) => {
-      const response = await fetch(
-        `https://arcane-scrubland-02167.herokuapp.com/api/${message.author.id}`
-      );
-      const json = await response.json().catch((error) => {
-        console.error(
-          `Character with api_id: '${message.author.id}' Not Found.`
-        );
-        return {
-          error: 404,
-          message: `Character with api_id: '${message.author.id}' Not Found.`,
-        };
-      });
-      const skill = args[0];
-      const result = await json;
-      const skill_rank = parseInt(result[skill]["rank"]);
-      const aspect_rank = parseInt(result[result[skill]["aspect"]]);
-      message.channel.send(skill + ":" + (skill_rank + aspect_rank));
-      console.log("done");
-    };
-    async_command(message, args);
-  },
-};
-bot.commands.set(get_character_command.name, get_character_command);
 
-//bot commands end -----------------------------
+//firebase functions --------------------------------
 
-function findSuit(string) {
+const findSuit = (string) => {
   char = string.charAt(0).toLowerCase();
   suit = "";
   switch (char) {
@@ -406,7 +76,7 @@ function findSuit(string) {
   return suit;
 }
 
-function createDeck(suits, values, center, descendant) {
+const createDeck = (suits, values, center, descendant) => {
   var deck = {};
   deck.cards = new Array();
   deck.discard = new Array();
@@ -444,7 +114,7 @@ function createDeck(suits, values, center, descendant) {
   return deck;
 }
 
-function shuffle(deck) {
+const shuffle = (deck) => {
   deck.cards = deck.cards.concat(deck.discard);
   deck.cards = deck.cards.concat(deck.hand);
   deck.discard = [];
@@ -458,103 +128,57 @@ function shuffle(deck) {
   console.log(deck.cards.length);
 }
 
-function writeDeck(authorID, deck, hand, discard) {
-  bot.decks[authorID] = {
-    cards: deck,
-    hand: hand,
-    discard: discard,
-  };
 
-  fs.writeFile("./decks.json", JSON.stringify(bot.decks, null, 4), (err) => {
-    if (err) throw err;
+
+const flip = async (authorID, numflips) => {
+  console.log(`flipping! ${authorID}`)
+  let deck;
+  let deckRef = usersRef.child(authorID)
+  
+  deckRef.on("value", async (snapshot) => {
+    console.log(snapshot.val());
+    deck = await snapshot.val();
+  }, async (errorObject) => {
+      console.log("error! " + errorObject.code);
+  })
+  console.log("decK: "+ deck);
+  
+  // for (var i = 0; i < numflips; i++) {
+  //   if (deck.cards.length <= 0) {
+  //     if (deck.discard.length <= 0) break;
+  //     deck.cards = deck.discard;
+  //     deck.discard = [];
+  //     shuffle(deck);
+  //   }
+  //   var pulledCard = deck.cards.shift();
+  //   flippedCards.unshift(pulledCard);
+  // }
+  // deck.discard = deck.discard.concat(flippedCards);
+  // return flippedCards;
+  
+}
+
+
+
+
+//end firebase functions --------------------------------
+
+
+
+const initializeFateDeck = async () => {
+  let fateDeckValues = createDeck(suits,values);
+  usersRef.child("fatedeck").set({
+    cards: fateDeckValues.cards,
+    hand: fateDeckValues.hand,
+    discard: fateDeckValues.discard
   });
 }
 
-//gets WHOLE deck as object from JSON. has: deck,hand,discard
-function readDeck(authorID) {
-  let deck = bot.decks[authorID];
-  return deck;
-}
 
-function draw(deck, numflips) {
-  var drawnCards = [];
-  for (var i = 0; i < numflips; i++) {
-    if (deck.cards.length <= 0) {
-      if (deck.discard.length <= 0) break;
-      deck.cards = deck.discard;
-      shuffle(deck);
-    }
-    var drawnCard = deck.cards.shift();
-    drawnCards.unshift(drawnCard);
-  }
-
-  deck.hand = deck.hand.concat(drawnCards);
-
-  deck.hand.sort((a, b) => {
-    return a.value - b.value;
-  });
-}
-
-function cheat(deck, cheatedValue) {
-  var cheatedCard = deck.hand.find((card) => {
-    return card.value === cheatedValue;
-  });
-  if (!cheatedCard) {
-    return cheatedCard;
-  }
-  deck.hand = deck.hand.filter((card) => {
-    return card.value !== cheatedValue;
-  });
-  deck.discard = deck.discard.concat(cheatedCard);
-
-  return cheatedCard;
-}
-
-function flip(deck, numflips) {
-  flippedCards = [];
-  for (var i = 0; i < numflips; i++) {
-    if (deck.cards.length <= 0) {
-      if (deck.discard.length <= 0) break;
-      deck.cards = deck.discard;
-      deck.discard = [];
-      shuffle(deck);
-    }
-    var pulledCard = deck.cards.shift();
-    flippedCards.unshift(pulledCard);
-  }
-  deck.discard = deck.discard.concat(flippedCards);
-  return flippedCards;
-}
-
-function isFM(id) {
-  return fatemaster_id === id;
-}
-
-const fetchData = async (message, args) => {
-  const response = await fetch(
-    `https://arcane-scrubland-02167.herokuapp.com/api/${message.author.id}`
-  );
-  const json = await response.json().catch((error) => {
-    console.error(`Character with api_id: '${message.author.id}' Not Found.`);
-    return {
-      error: 404,
-      message: `Character with api_id: '${message.author.id}' Not Found.`,
-    };
-  });
-  return json;
-};
-
-bot.once("ready", () => {
-  let fate_deck = readDeck(0);
-  if (fate_deck) {
-    console.log("bot running, deck already existed");
-  } else {
-    console.log("fate deck doesn't exist");
-    fate_deck = createDeck(suits, values);
-    writeDeck(0, fate_deck.cards, fate_deck.hand, fate_deck.discard);
-    console.log("fate deck initialized");
-  }
+bot.once("ready", async () => {
   bot.user.setActivity('type "!help" for a list of commands');
+  console.log("ready!");
+  flip("fatedeck",1)
 });
 
 bot.on("message", async (message) => {
